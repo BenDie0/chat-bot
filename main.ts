6c929227b570360e807734a1ec000279b61dbcf8
BenDie0 <58610677+BenDie0@users.noreply.github.com>
2020-02-10 14:01:47 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+if (game.ask("whats your name?")) {
+    game.splash(game.askForString("whats your name?"))
+}
+if (game.ask("whats your favorite color?")) {
+    game.splash(game.askForString("whats your favorite color?"))
+}
+if (game.ask("do you like dogs or cats?")) {
+    game.splash(game.askForString("do you eat dog or cat?"))
+}
